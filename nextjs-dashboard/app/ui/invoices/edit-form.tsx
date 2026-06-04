2816cd2cc82ef7699dbe5528a87c7cb5e975e131
Pia <mpiabarcelo@gmail.com>
2026-06-04 10:39:22 +0200

    TODO: terminar edit invoices

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
+
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -18,7 +19,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   return (
-    <form>
+  <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
